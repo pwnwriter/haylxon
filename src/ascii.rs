a121020e7008fb7bd26724157bf78e5f3135f08a
pwnwriter <hey@pwnwriter.xyz>
2023-03-23 11:32:59 +0545

    [Feat]: shortened binary name 🐶 + add colors 🎨, new ASCII ✨

--- src/ascii.rs
+++ src/ascii.rs
@@ -1,7 +1,19 @@
-pub const HYALCON: &str = r"
-╦ ╦┌─┐┬ ┬┬  ┌─┐┌─┐┌┐┌
-╠═╣├─┤└┬┘│  │  │ ││││
-╩ ╩┴ ┴ ┴ ┴─┘└─┘└─┘┘└┘
- Eyes on the blink  
+//pub const HYALCON: &str = r"
+//╦ ╦┌─┐┬ ┬┬  ┌─┐┌─┐┌┐┌
+//╠═╣├─┤└┬┘│  │  │ ││││
+//╩ ╩┴ ┴ ┴ ┴─┘└─┘└─┘┘└┘v1.0.0
+//              by @pwnwriter
+//";
+
+pub const BAR: &str = r"
+────────────────────────────────
 ";
 
+
+pub const HXN: &str = r"
+╦ ╦╔═╗╦ ╦╦  ═╗ ╦╔═╗╔╗╔
+╠═╣╠═╣╚╦╝║  ╔╩╦╝║ ║║║║
+╩ ╩╩ ╩ ╩ ╩═╝╩ ╚═╚═╝╝╚╝v0.1.0
+ Shoot before the blink  
+            by @PwnWriter
+";
