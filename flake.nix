1bc7a7db20ca4db06da40afc3661139c944d09af
pwnwriter <hey@pwnwriter.xyz>
2024-03-26 05:15:23 -0400

    feat(nix): dev flake for cc build

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "haylxon dev flake";
+  inputs =
+    {
+      nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    };
+
+  outputs = { self, nixpkgs, ... }@inputs:
+    let
+      system = "aarch64-darwin";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell
+        {
+          packages = with pkgs; [ rustup ];
+        };
+    };
+}
+
