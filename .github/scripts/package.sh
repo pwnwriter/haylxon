7ca18d89041f9bb50342e90f1e15816cff523929
pwnwriter <hi@pwnwriter.me>
2026-06-09 02:29:37 -0400

    chore(pkg): use sha12sum for winbows

--- .github/scripts/package.sh
+++ .github/scripts/package.sh
@@ -9,6 +9,14 @@
 #   haylxon-<version>-<target>.zip    + .sha512  (windows)
 set -euo pipefail
 
+sha512() {
+    if command -v shasum &>/dev/null; then
+        shasum -a 512 "$1"
+    else
+        sha512sum "$1"
+    fi
+}
+
 VERSION="$1"
 TARGET="$2"
 NAME="haylxon-${VERSION}"
@@ -21,11 +29,11 @@ cp LICENSE README.md "$NAME/"
 if [[ "$TARGET" == *windows* ]]; then
     cp "target/${TARGET}/release/hxn.exe" "$NAME/"
     7z a -tzip "${ARCHIVE_BASE}.zip" "$NAME"
-    shasum -a 512 "${ARCHIVE_BASE}.zip" > "${ARCHIVE_BASE}.zip.sha512"
+    sha512 "${ARCHIVE_BASE}.zip" > "${ARCHIVE_BASE}.zip.sha512"
 else
     cp "target/${TARGET}/release/hxn" "$NAME/"
     tar -czvf "${ARCHIVE_BASE}.tar.gz" "$NAME/"
-    shasum -a 512 "${ARCHIVE_BASE}.tar.gz" > "${ARCHIVE_BASE}.tar.gz.sha512"
+    sha512 "${ARCHIVE_BASE}.tar.gz" > "${ARCHIVE_BASE}.tar.gz.sha512"
 fi
 
 rm -rf "$NAME"
